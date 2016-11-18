a70e522e7eb34deddddd5c61dbf9c563bbf7ab98
“3HR717” <tumholtz@fullsail.edu>
2016-11-18 09:06:56 +0800

    Assignment 02 - Party List Solution

---
+++ UmholtzTomas_Assignment02/UmholtzTomas_Assignment02a/UmholtzTomas_Assignment02a/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace UmholtzTomas_Assignment02a
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlContainer = new System.Windows.Forms.Panel();
+            this.picDelete = new System.Windows.Forms.PictureBox();
+            this.picLeft = new System.Windows.Forms.PictureBox();
+            this.picRight = new System.Windows.Forms.PictureBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnNeed = new System.Windows.Forms.Button();
+            this.btnHave = new System.Windows.Forms.Button();
+            this.txtItem = new System.Windows.Forms.TextBox();
+            this.lstNeed = new System.Windows.Forms.ListBox();
+            this.lstHave = new System.Windows.Forms.ListBox();
+            this.lblNeed = new System.Windows.Forms.Label();
+            this.lblHave = new System.Windows.Forms.Label();
+            this.lblPartyList = new System.Windows.Forms.Label();
+            this.pnlContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picDelete)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picRight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlContainer
+            // 
+            this.pnlContainer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlContainer.Controls.Add(this.picDelete);
+            this.pnlContainer.Controls.Add(this.picLeft);
+            this.pnlContainer.Controls.Add(this.picRight);
+            this.pnlContainer.Controls.Add(this.btnLoad);
+            this.pnlContainer.Controls.Add(this.btnSave);
+            this.pnlContainer.Controls.Add(this.btnNeed);
+            this.pnlContainer.Controls.Add(this.btnHave);
+            this.pnlContainer.Controls.Add(this.txtItem);
+            this.pnlContainer.Controls.Add(this.lstNeed);
+            this.pnlContainer.Controls.Add(this.lstHave);
+            this.pnlContainer.Controls.Add(this.lblNeed);
+            this.pnlContainer.Controls.Add(this.lblHave);
+            this.pnlContainer.Controls.Add(this.lblPartyList);
+            this.pnlContainer.Location = new System.Drawing.Point(337, 111);
+            this.pnlContainer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlContainer.Name = "pnlContainer";
+            this.pnlContainer.Size = new System.Drawing.Size(641, 643);
+            this.pnlContainer.TabIndex = 0;
+            // 
+            // picDelete
+            // 
+            this.picDelete.BackColor = System.Drawing.Color.Transparent;
+            this.picDelete.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picDelete.Image = global::UmholtzTomas_Assignment02a.Properties.Resources.Untitled_2_02;
+            this.picDelete.Location = new System.Drawing.Point(284, 316);
+            this.picDelete.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.picDelete.Name = "picDelete";
+            this.picDelete.Size = new System.Drawing.Size(44, 38);
+            this.picDelete.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picDelete.TabIndex = 12;
+            this.picDelete.TabStop = false;
+            this.picDelete.Click += new System.EventHandler(this.picDelete_Click);
+            // 
+            // picLeft
+            // 
+            this.picLeft.BackColor = System.Drawing.Color.Transparent;
+            this.picLeft.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picLeft.Image = global::UmholtzTomas_Assignment02a.Properties.Resources.Untitled_2_01;
+            this.picLeft.Location = new System.Drawing.Point(284, 256);
+            this.picLeft.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.picLeft.Name = "picLeft";
+            this.picLeft.Size = new System.Drawing.Size(44, 38);
+            this.picLeft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picLeft.TabIndex = 11;
+            this.picLeft.TabStop = false;
+            this.picLeft.Click += new System.EventHandler(this.picLeft_Click);
+            // 
+            // picRight
+            // 
+            this.picRight.BackColor = System.Drawing.Color.Transparent;
+            this.picRight.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picRight.Image = global::UmholtzTomas_Assignment02a.Properties.Resources.Untitled_2_03;
+            this.picRight.Location = new System.Drawing.Point(284, 196);
+            this.picRight.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.picRight.Name = "picRight";
+            this.picRight.Size = new System.Drawing.Size(44, 38);
+            this.picRight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picRight.TabIndex = 10;
+            this.picRight.TabStop = false;
+            this.picRight.Click += new System.EventHandler(this.picRight_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoad.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoad.Location = new System.Drawing.Point(456, 555);
+            this.btnLoad.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(133, 38);
+            this.btnLoad.TabIndex = 9;
+            this.btnLoad.Text = "LOAD";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(315, 555);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(133, 38);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnNeed
+            // 
+            this.btnNeed.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNeed.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNeed.Location = new System.Drawing.Point(384, 490);
+            this.btnNeed.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnNeed.Name = "btnNeed";
+            this.btnNeed.Size = new System.Drawing.Size(133, 38);
+            this.btnNeed.TabIndex = 7;
+            this.btnNeed.Text = "NEED";
+            this.btnNeed.UseVisualStyleBackColor = true;
+            this.btnNeed.Click += new System.EventHandler(this.btnNeed_Click);
+            // 
+            // btnHave
+            // 
+            this.btnHave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHave.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHave.Location = new System.Drawing.Point(91, 490);
+            this.btnHave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnHave.Name = "btnHave";
+            this.btnHave.Size = new System.Drawing.Size(133, 38);
+            this.btnHave.TabIndex = 6;
+            this.btnHave.Text = "HAVE";
+            this.btnHave.UseVisualStyleBackColor = true;
+            this.btnHave.Click += new System.EventHandler(this.btnHave_Click);
+            // 
+            // txtItem
+            // 
+            this.txtItem.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtItem.Location = new System.Drawing.Point(147, 450);
+            this.txtItem.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtItem.Name = "txtItem";
+            this.txtItem.Size = new System.Drawing.Size(314, 22);
+            this.txtItem.TabIndex = 5;
+            // 
+            // lstNeed
+            // 
+            this.lstNeed.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lstNeed.FormattingEnabled = true;
+            this.lstNeed.ItemHeight = 16;
+            this.lstNeed.Location = new System.Drawing.Point(364, 159);
+            this.lstNeed.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstNeed.Name = "lstNeed";
+            this.lstNeed.ScrollAlwaysVisible = true;
+            this.lstNeed.Size = new System.Drawing.Size(185, 258);
+            this.lstNeed.TabIndex = 4;
+            // 
+            // lstHave
+            // 
+            this.lstHave.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lstHave.FormattingEnabled = true;
+            this.lstHave.ItemHeight = 16;
+            this.lstHave.Location = new System.Drawing.Point(68, 159);
+            this.lstHave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstHave.Name = "lstHave";
+            this.lstHave.ScrollAlwaysVisible = true;
+            this.lstHave.Size = new System.Drawing.Size(185, 258);
+            this.lstHave.TabIndex = 3;
+            // 
+            // lblNeed
+            // 
+            this.lblNeed.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblNeed.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNeed.Location = new System.Drawing.Point(384, 92);
+            this.lblNeed.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNeed.Name = "lblNeed";
+            this.lblNeed.Size = new System.Drawing.Size(133, 35);
+            this.lblNeed.TabIndex = 2;
+            this.lblNeed.Text = "NEED";
+            this.lblNeed.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblHave
+            // 
+            this.lblHave.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblHave.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHave.Location = new System.Drawing.Point(91, 92);
+            this.lblHave.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblHave.Name = "lblHave";
+            this.lblHave.Size = new System.Drawing.Size(133, 35);
+            this.lblHave.TabIndex = 1;
+            this.lblHave.Text = "HAVE";
+            this.lblHave.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPartyList
+            // 
+            this.lblPartyList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblPartyList.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPartyList.Location = new System.Drawing.Point(244, 28);
+            this.lblPartyList.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPartyList.Name = "lblPartyList";
+            this.lblPartyList.Size = new System.Drawing.Size(133, 35);
+            this.lblPartyList.TabIndex = 0;
+            this.lblPartyList.Text = "Party List";
+            this.lblPartyList.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1344, 897);
+            this.Controls.Add(this.pnlContainer);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Party List";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlContainer.ResumeLayout(false);
+            this.pnlContainer.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picDelete)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picRight)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlContainer;
+        private System.Windows.Forms.Label lblNeed;
+        private System.Windows.Forms.Label lblHave;
+        private System.Windows.Forms.Label lblPartyList;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnNeed;
+        private System.Windows.Forms.Button btnHave;
+        private System.Windows.Forms.TextBox txtItem;
+        private System.Windows.Forms.ListBox lstNeed;
+        private System.Windows.Forms.ListBox lstHave;
+        private System.Windows.Forms.PictureBox picRight;
+        private System.Windows.Forms.PictureBox picDelete;
+        private System.Windows.Forms.PictureBox picLeft;
+    }
+}
+
---
+++ UmholtzTomas_Assignment02/UmholtzTomas_Assignment02a/UmholtzTomas_Assignment02a/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace UmholtzTomas_Assignment02a
+{
+    public partial class Form1 : Form
+    {
+        ArrayList lsHave = new ArrayList();
+        ArrayList lsNeed = new ArrayList();
+        string txtToAppend = "> ";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Moving selected item from HAVE list to NEED list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void picRight_Click(object sender, EventArgs e)
+        {
+            if (lstHave.SelectedIndex >= 0)
+            {
+                lsNeed.Add(txtToAppend + lstHave.SelectedItem.ToString().Split(' ')[1]);
+                lsHave.Remove(lstHave.SelectedItem);
+                lstHave.DataSource = null;
+                lstNeed.DataSource = null;
+                lstHave.DataSource = lsHave;
+                lstNeed.DataSource = lsNeed;
+            }
+            else
+                MessageBox.Show("Please select an item to move.");
+        }
+        /// <summary>
+        /// Moving selected item from NEED to HAVE
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void picLeft_Click(object sender, EventArgs e)
+        {
+            if (lstNeed.SelectedIndex >= 0)
+            {
+                lsHave.Add(txtToAppend + lstNeed.SelectedItem.ToString().Split(' ')[1]);
+                lsNeed.Remove(lstNeed.SelectedItem);
+                lstHave.DataSource = null;
+                lstNeed.DataSource = null;
+                lstNeed.DataSource = lsNeed;
+                lstHave.DataSource = lsHave;
+            }
+            else
+                MessageBox.Show("Please select an item to move.");
+        }
+        /// <summary>
+        /// Deleting selected item from NEED & HAVE list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void picDelete_Click(object sender, EventArgs e)
+        {
+            if (lstNeed.SelectedIndex >= 0)
+                lsNeed.Remove(lstNeed.SelectedItem);
+
+            if (lstHave.SelectedIndex >= 0)
+                lsHave.Remove(lstHave.SelectedItem);
+
+            lstHave.DataSource = null;
+            lstNeed.DataSource = null;
+            lstNeed.DataSource = lsNeed;
+            lstHave.DataSource = lsHave;
+        }
+        /// <summary>
+        /// Adding entered text to HAVE list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnHave_Click(object sender, EventArgs e)
+        {
+            if (txtItem.Text != "")
+            {
+                lsHave.Add(txtToAppend + txtItem.Text);
+                txtItem.Text = "";
+                lstHave.DataSource = null;
+                lstHave.DataSource = lsHave;
+            }
+            else
+                MessageBox.Show("Please enter text to add");
+        }
+        /// <summary>
+        /// Adding entered text to NEED list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnNeed_Click(object sender, EventArgs e)
+        {
+            if (txtItem.Text != "")
+            {
+                lsNeed.Add(txtToAppend + txtItem.Text);
+                txtItem.Text = "";
+                lstNeed.DataSource = null;
+                lstNeed.DataSource = lsNeed;
+            }
+            else
+                MessageBox.Show("Please enter text to add");
+        }
+        /// <summary>
+        /// Saving list items to XML
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sv = new SaveFileDialog();
+            if (sv.ShowDialog() == DialogResult.OK)
+            {
+                string filename = "";
+                filename = sv.FileName.IndexOf('.') > 0 ? sv.FileName.Substring(0, sv.FileName.IndexOf('.')) + ".xml" : sv.FileName + ".xml";
+
+                XmlDocument xmlDoc = new XmlDocument();
+                XmlNode rootNode = xmlDoc.CreateElement("PartyList");
+                xmlDoc.AppendChild(rootNode);
+                for (int i = 0; i < lstHave.Items.Count; i++)
+                {
+                    XmlNode userNode = xmlDoc.CreateElement("Item");
+                    XmlAttribute attribute = xmlDoc.CreateAttribute("listType");
+                    attribute.Value = "Have";
+                    userNode.Attributes.Append(attribute);
+                    userNode.InnerText = lstHave.Items[i].ToString();
+                    rootNode.AppendChild(userNode);
+                }
+                for (int i = 0; i < lstNeed.Items.Count; i++)
+                {
+                    XmlNode userNode = xmlDoc.CreateElement("Item");
+                    XmlAttribute attribute = xmlDoc.CreateAttribute("listType");
+                    attribute.Value = "Need";
+                    userNode.Attributes.Append(attribute);
+                    userNode.InnerText = lstNeed.Items[i].ToString();
+                    rootNode.AppendChild(userNode);
+                }
+                xmlDoc.Save(filename);
+                if(MessageBox.Show("Party list saved successfully!!!")==DialogResult.OK)
+                {
+                    lsHave.Clear();
+                    lsNeed.Clear();
+                    lstHave.DataSource = null;
+                    lstNeed.DataSource = null;
+                    lstHave.DataSource = lsHave;
+                    lstNeed.DataSource = lsNeed;
+                }
+            }
+        }
+        /// <summary>
+        /// Loading xml to load HAVE & NEED list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                lsHave.Clear();
+                lsNeed.Clear();
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(op.FileName);
+                XmlNodeList userNodes = xmlDoc.SelectNodes("//PartyList/Item");
+                foreach (XmlNode userNode in userNodes)
+                {
+                    if (userNode.Attributes["listType"].Value == "Have")
+                        lsHave.Add(userNode.InnerText);
+                    else
+                        lsNeed.Add(userNode.InnerText);
+                }
+                lstHave.DataSource = null;
+                lstNeed.DataSource = null;
+                lstHave.DataSource = lsHave;
+                lstNeed.DataSource = lsNeed;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
